804489f9dcdfe03aa32ea9a602860061de670cf2
Pravin Paratey <pravin@paratey.com>
2022-07-17 19:53:04 +0100

    Restructure

--- src/game.rs
+++ src/game.rs
@@ -6,7 +6,7 @@ use pancurses::{
 pub fn initgame() {
     let win: Window = initscr();
 
-    if win.get_max_y() < 20 || win.get_max_x() < 30 {
+    if win.get_max_y() < 20 || win.get_max_x() < 50 {
         panic!("You need at least a 30x20 terminal window to play this game.");
     }
 
@@ -24,18 +24,19 @@ pub fn initgame() {
     cbreak();
     noecho();
 
-    draw_header(&win, win.get_max_x());
-    draw_keyboard(&win, win.get_max_x(), win.get_max_y());
+    draw_header(&win);
+    draw_keyboard(&win);
+    draw_footer(&win);
 
     win.getch();
     endwin();
 }
 
-fn draw_header(win: &Window, width: i32) {
+fn draw_header(win: &Window) {
     const TITLE: &str = "cwordle";
     let title_len: i32 = TITLE.chars().count() as i32;
     let color_array = [3, 2, 4, 4, 3, 2, 3].to_vec();
-    let mut x_pos = (width - (title_len * 4)) / 2;
+    let mut x_pos = (win.get_max_x() - (title_len * 4)) / 2;
 
     for (i, c) in TITLE.chars().enumerate() {
         win.attrset(COLOR_PAIR(color_array[i]));
@@ -45,20 +46,28 @@ fn draw_header(win: &Window, width: i32) {
     win.attrset(COLOR_PAIR(1)); // reset colors
 }
 
-fn draw_keyboard(win: &Window, width: i32, height: i32) {
+fn draw_keyboard(win: &Window) {
     let keyboard = "qwertyuiopasdfghjklzxcvbnm";
-    let mut x_pos: i32 = (width - 18) / 2; // 9 letters per line
-    let mut y_pos: i32 = height - 3;
+    let mut x_pos: i32 = (win.get_max_x() - 36) / 2; // 9 letters per line
+    let mut y_pos: i32 = win.get_max_y() - 6;
     for j in 0..26 {
-        win.mvaddch(y_pos, x_pos, keyboard.as_bytes()[j] as char);
-        x_pos += 2;
+        win.mvaddstr(
+            y_pos,
+            x_pos,
+            format!(" {} ", keyboard.as_bytes()[j] as char),
+        );
+        x_pos += 4;
         if (j + 1) % 10 == 0 {
-            y_pos += 1;
-            x_pos = (width - 18) / 2;
+            y_pos += 2;
+            x_pos = (win.get_max_x() - 36) / 2;
             if j == 19 {
                 // center the last line
-                x_pos += 4;
+                x_pos += 8;
             }
         }
     }
+}
+
+fn draw_footer(win: &Window) {
+    win.mvaddstr(win.get_max_y() - 1, 2, " Press ? for help ");
 }
