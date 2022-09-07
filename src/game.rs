7e717006364990a992fd1d917ea278078dff8b33
Pravin Paratey <pravin@kaizan.ai>
2022-09-07 12:37:31 +0100

    More progress

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use pancurses::{
     cbreak, endwin, has_colors, init_pair, initscr, noecho, start_color, use_default_colors, Input,
-    Window, COLOR_BLACK, COLOR_GREEN, COLOR_PAIR, COLOR_WHITE, COLOR_YELLOW,
+    Window, COLOR_BLACK, COLOR_BLUE, COLOR_GREEN, COLOR_PAIR, COLOR_WHITE, COLOR_YELLOW,
 };
 
 pub fn init_game() -> Window {
@@ -20,6 +20,7 @@ pub fn init_game() -> Window {
     init_pair(2, COLOR_BLACK, COLOR_WHITE);
     init_pair(3, COLOR_BLACK, COLOR_GREEN);
     init_pair(4, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(5, COLOR_WHITE, COLOR_BLUE);
 
     cbreak();
     noecho();
@@ -77,14 +78,44 @@ pub fn game_loop(win: &Window) {
     let mut winner = false;
     let mut count = 0;
     while !winner && count < 6 {
-        let word = get_valid_word(win);
-        //let get_input();
+        let word = get_valid_word(win, count);
         count += 1;
     }
 }
 
-fn get_valid_word(win: &Window) {
-    let c = win.getch();
+fn get_valid_word(win: &Window, row: i32) -> String {
+    let mut count: i32 = 0;
+    let x_pos: i32 = (win.get_max_x() - 20) / 2; // 20 = 5 chars * 4 spaces for each char
+    let y_pos: i32 = (win.get_max_y() / 2) - 8 + row * 2; // 6 rows * 2 spaces for each row
+    let mut input_array: [char; 5] = [' ', ' ', ' ', ' ', ' '];
+
+    win.attrset(COLOR_PAIR(5));
+    loop {
+        match win.getch() {
+            Some(Input::Character(ch)) => {
+                if ch == '\n' && count == 5 {
+                    // Enter Key
+                    break;
+                }
+                if !ch.is_alphabetic() || count >= 5 {
+                    continue;
+                }
+                input_array[count as usize] = ch;
+                win.mvaddstr(y_pos, count * 4 + x_pos, format!(" {} ", ch));
+            }
+            Some(Input::KeyEnter) => {
+                win.printw("HELLO");
+            }
+            Some(_input) => continue,
+            None => continue,
+        }
+        count += 1;
+    }
+    win.attrset(COLOR_PAIR(1)); // Reset colors
+
+    // Check if word is valid
+    let input_word: String = input_array.iter().collect();
+    input_word
 }
 
 pub fn end_game(win: &Window) {
